c0e48830e3edb1076f704f3f9005ef7deca7f9c3
Elian Paredes <elianparedes.dev@gmail.com>
2023-03-25 00:59:34 -0300

    build: project setup

---
+++ webapp/src/main/java/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/")
+    public ModelAndView helloWorld() {
+        final ModelAndView mav = new ModelAndView("index");
+        mav.addObject("greeting", "PAW");
+        return mav;
+    }
+}
